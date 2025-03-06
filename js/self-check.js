6e4c7b6fa8dba579835f533c2fce73f9f7c62c50
unknown <semmi.ezaitouni@kuleuven.be>
2025-03-06 11:13:56 +0100

    added log out  after inactivity, fixed bug where when renewed loan, immediatly see new due date, ready for testing

--- js/self-check.js
+++ js/self-check.js
@@ -3,6 +3,8 @@ var baseURL = "https://services.t.libis.be";
 var libraryName = "RBIB";
 var circDesk = "DEFAULT_CIRC_DESK";
 
+var isLoggedIn = false; // Flag to track login state
+
 function initiate() {
 	getModalBox();
 	
@@ -58,12 +60,42 @@ function returnToBarcode() {
 	$("#barcode").focus();
 }
 
+var inactivityTime = function () {
+    var time;
+    var logoutTime = 30000; // 30 seconds
+
+    function resetTimer() {
+        if (isLoggedIn) { // Only reset the timer if the user is logged in
+            clearTimeout(time);
+            time = setTimeout(logoutUser, logoutTime);
+        }
+    }
+
+    // Events to reset the timer
+    window.onload = resetTimer;
+    document.onmousemove = resetTimer;
+    document.onkeypress = resetTimer;
+    document.ontouchstart = resetTimer; 
+
+    // Logout function
+    function logoutUser() {
+        if (isLoggedIn) { // Only log out if the user is logged in
+            logout();
+            returnToBarcode();
+            isLoggedIn = false;
+        }
+    }
+};
+
+// Initialize the inactivity timer
+inactivityTime();
+
 
 /* LOGIN */
-
 function login() {
     var loginid = $("#userid").val();
     if ((loginid != null) && (loginid != "")) {
+        isLoggedIn = true;
         
         $("#userid").prop("disabled", true);
         $("#loginerror").addClass("hide");
@@ -81,8 +113,6 @@ function login() {
             
         }).done(function(data) {
             user = data;
-            console.log("data:", data);
-
             // Prepare scan box
             $("#scanboxtitle").text("Welcome " + data.first_name + " " + data.last_name);
             $("#userloans").text(data.loans.value);
@@ -103,7 +133,8 @@ function login() {
                 loanData.item_loan.forEach(function(loan) {
                     var dueDate = new Date(loan.due_date);
                     var dueDateText = (dueDate.getMonth() + 1) + "/" + dueDate.getDate() + "/" + dueDate.getFullYear();
-                    $("#loanstable").append("<tr><td>" + loan.title + "</td><td>" + dueDateText + "</td><td><button onclick='renewLoan(\"" + loan.loan_id + "\")'>RENEW</button></td></tr>");
+                    $("#loanstable").append("<tr id='loan-" + loan.loan_id + "'><td>" + loan.title + "</td><td>" + dueDateText + "</td><td><button onclick='renewLoan(\"" + loan.loan_id + "\")'>RENEW</button></td></tr>");
+                    returnToBarcode();
                 });
             }).fail(function(jqxhr, textStatus, error) {
                 console.log(jqxhr.responseText);
@@ -152,12 +183,9 @@ function loan() {
     		var dueDate = new Date($(data).find("due_date").text());
     		var dueDateText = (parseInt(dueDate.getMonth()) + 1) + "/" + dueDate.getDate() + "/" + dueDate.getFullYear();
     		$("#loanstable").append("<tr id='loan-" + loan.loan_id + "'><td>" + $(data).find("title").text() + "</td><td>" + dueDateText + "</td></tr>");
-    		
     		returnToBarcode();
-    		
     	}).fail(function(jqxhr, textStatus, error) {
     		console.log(jqxhr.responseText);
-    		
     		$("#modalheader").text("");
     		$("#modalheader").append("item not avaiable for loan.<br/><br/>please see the reference desk for more information<br/><br/><input class='modalclose' type='button' value='close' id='barcodeerrorbutton' onclick='javascript:returnToBarcode();'/>");
     		$("#barcodeerrorbutton").focus();
@@ -173,13 +201,10 @@ function loan() {
     }
 } 
 function renewLoan(loanId) {
-    console.log( "'loanId'" ,loanId)
     const userId = user.primary_id;
-
     $("#modalheader").text("processing renewal, please wait...");
     $("#myModal").show();
     $(".close").hide();
-
     $.ajax({
         type: "POST",
         url: baseURL + "/almaws/v1/users/" + userId + "/loans/" + loanId + "?apikey=" + API_KEY + "&op=renew",
@@ -187,10 +212,28 @@ function renewLoan(loanId) {
         data: "<?xml version='1.0' encoding='UTF-8'?><renew_loan><circ_desk>" + circDesk + "</circ_desk><library>" + libraryName + "</library></renew_loan>",
         dataType: "xml"
     }).done(function(data) {
-        var dueDate = new Date($(data).find("due_date").text());
-        var dueDateText = (parseInt(dueDate.getMonth()) + 1) + "/" + dueDate.getDate() + "/" + dueDate.getFullYear();
-        $("#loan-" + loanId).find(".due-date").text(dueDateText);
-        $("#myModal").hide();
+        // var dueDate = new Date($(data).find("due_date").text());
+        // var dueDateText = (parseInt(dueDate.getMonth()) + 1) + "/" + dueDate.getDate() + "/" + dueDate.getFullYear();
+        // Make a new GET request to fetch the updated loan data to fix bug of not seeing immediatly
+        $.ajax({
+            type: "GET",
+            url: baseURL + "/almaws/v1/users/" + userId + "/loans/" + loanId + "?apikey=" + API_KEY,
+            contentType: "application/json",
+            dataType: "json"
+        }).done(function(updatedLoanData) {
+            var updatedDueDate = new Date(updatedLoanData.due_date);
+            var updatedDueDateText = (updatedDueDate.getMonth() + 1) + "/" + updatedDueDate.getDate() + "/" + updatedDueDate.getFullYear();
+
+            // Update the due date in the table
+            var dueDateCell = $("#loan-" + loanId + " td:nth-child(2)");
+            dueDateCell.text(updatedDueDateText);
+            $("#myModal").hide();
+        }).fail(function(error) {
+            console.error('Error fetching updated loan data:', error);
+            alert('Failed to fetch updated loan data');
+            $("#myModal").hide();
+        });
+        
     }).fail(function(error) {
         console.error('Error:', error);
         alert('Failed to renew loan');
